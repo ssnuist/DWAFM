6bf7dde316a2c83962d8eaa10f520a9829e82e1b
ssnuist <202312380032@nuist.edu.cn>
2025-08-29 13:27:26 +0800

    Delete easytorch directory

--- easytorch/core/__init__.py
+++
@@ -1,7 +0,0 @@
-from .runner import Runner
-from .meter_pool import AvgMeter, MeterPool
-
-
-__all__ = [
-    'Runner', 'AvgMeter', 'MeterPool'
-]
--- easytorch/launcher/__init__.py
+++
@@ -1,4 +0,0 @@
-from .launcher import launch_runner, launch_training
-from .dist_wrap import dist_wrap
-
-__all__ = ['launch_runner', 'launch_training', 'dist_wrap']
